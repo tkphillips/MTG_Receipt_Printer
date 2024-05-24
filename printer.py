fd0fda01050499d788f6fc9b22c5b4be95a00815
Tristan Phillips <phillipskalani@gmail.com>
2024-05-24 23:22:38 +0100

    Added code to get random card image off scryfall

--- printer.py
+++ printer.py
@@ -1,8 +1,18 @@
 import serial
+import random
+import json
+import requests
 from Adafruit_Thermal import *
-
 printer = Adafruit_Thermal("/dev/ttyAMA0", 9600, timeout=5)
 
-# Print the 75x75 pixel logo in adalogo.py
-import gfx.adalogo as adalogo
-printer.printBitmap(adalogo.width, adalogo.height, adalogo.data)
+AllCardsJson = requests.get("https://api.scryfall.com/catalog/card-names")
+
+randomCard = random.choice(AllCardsJson.json()["data"])
+randomCard.replace(" ", "+")
+randomCardImage = requests.get("https://api.scryfall.com/cards/named?exact=" + randomCard + "&format=image&version=border_crop").content
+with open('image_name.jpg', 'wb') as handler:
+    handler.write(randomCardImage)
+
+## Print the Card
+#import gfx.adalogo as adalogo
+#printer.printBitmap(adalogo.width, adalogo.height, adalogo.data)
