07c617f091eefe18b35816701c882067f05368df
Tristan Phillips <phillipskalani@gmail.com>
2024-05-27 02:56:40 +0100

    Working! need to add dithering

--- printer.py
+++ printer.py
@@ -10,6 +10,7 @@ import numpy as np
 import io
 from PIL import Image, ImageOps
 
+
 printer = Adafruit_Thermal("/dev/ttyAMA0", 9600, timeout=5)
 
 AllCardsJson = requests.get("https://api.scryfall.com/catalog/card-names")
@@ -22,7 +23,7 @@ with open('temp_img.jpg', 'wb') as handler:
 
 
 img = Image.open("temp_img.jpg")
-newsize = (380, 530)
+newsize = (384, 536)
 img = img.resize(newsize)
 img = ImageOps.invert(img)
 arr = np.array(img)
@@ -34,14 +35,22 @@ b = b.reshape(-1)
 
 bmp = list(map(lambda x: 0.299*x[0]+0.587*x[1]+0.114*x[2],zip(r,g,b)))
 bmp = np.array(bmp).reshape([arr.shape[0], arr.shape[1]])
-bmp = np.dot((bmp > 128).astype(int), 255)
-bmp = bmp.astype(np.uint8).tolist()
+bmp = np.dot((bmp > 128).astype(int), 255).flatten()
+
+bmp = bmp.astype(np.bool_).tolist()
+
+final = []
+for x in range(0, len(bmp), 8):
+    temp = bmp[x:x+8]
+    n = int(''.join(['1' if i else '0' for i in temp
+                     ]), 2)
+    final.append(n)
 
 with open('your_file.txt', 'w') as f:
-    for line in bmp:
+    for line in final:
         f.write(f"{line}\n")
 
 
-print(bmp)
+print(final)
 ## Print the Card
-printer.printBitmap(530, 380, bmp)
+printer.printBitmap(384, 536, final)
